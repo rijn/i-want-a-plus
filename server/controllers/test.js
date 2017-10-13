777a5d9efccab82fe0bbae2a74dde32d0190ac30
jz <jz@wirelessprv-10-194-55-134.near.illinois.edu>
2017-10-12 20:59:55 -0500

    test push

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -5,7 +5,10 @@ const sequelize = require('../models').sequelize;
 module.exports = (req, res) => {
     return Q()
         .then(() => {
-            return sequelize.query('SELECT `SID` FROM `Students`;', { type: sequelize.QueryTypes.SELECT });
+            return sequelize.query(
+                'SELECT `SID` FROM `Students`;',
+                { type: sequelize.QueryTypes.SELECT }
+            );
         })
         .done(sids => {
             res.status(201).send(sids);
